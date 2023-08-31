6caa01c2788896e9244c2ccf1d811970c2254e73
Frederick Adebayo <sprintcorp7@gmail.com>
2023-08-31 20:24:07 +0100

    created user and order table

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const post1 = await prisma.book.upsert({
+        where: { id: 1 },
+        update: {},
+        create: {
+        //   id: 1,
+          title: 'Book one',
+          writer: 'Fred Adebayo',
+          coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51Ga5GuElyL._AC_SX184_.jpg',
+          point: 100,
+          tags: ['fiction', 'non-fiction', 'science', 'essay'],
+        },
+      });
+
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
