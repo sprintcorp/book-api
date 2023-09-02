d299faae05d988363cf547edc246e1efe986be4d
Frederick Adebayo <sprintcorp7@gmail.com>
2023-09-02 14:31:21 +0100

    rabbitMQ request logger

--- src/middlewares/logging.middleware.ts
+++ src/middlewares/logging.middleware.ts
@@ -1,5 +1,3 @@
-// src/middleware/rabbitmq-logger.middleware.ts
-
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { LoggingService } from 'src/services/logger.service';
@@ -9,7 +7,6 @@ export class RabbitMqLoggerMiddleware implements NestMiddleware {
   constructor(private readonly loggingService: LoggingService) {}
 
   async use(req: Request, res: Response, next: () => void) {
-    // Log the request to RabbitMQ before passing it to the route handler
     await this.loggingService.logRequest({
       method: req.method,
       url: req.originalUrl,
