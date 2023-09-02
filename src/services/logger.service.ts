e2a3b5fdce9e7ed1bcb0c47cb96e097eeb716082
Frederick Adebayo <sprintcorp7@gmail.com>
2023-09-02 14:30:28 +0100

    rabbitMQ request logger

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -1,11 +1,11 @@
-// src/logging/logging.service.ts
-
 import { Injectable } from '@nestjs/common';
 import * as amqp from 'amqplib';
 
 @Injectable()
 export class LoggingService {
   private readonly rabbitMqUrl = 'amqps://scfwsotk:f52LYqeskoX8pUYbr-8Il9wlYy8-yTEI@crow.rmq.cloudamqp.com/scfwsotk'; // Update with your RabbitMQ server URL
+
+  
 
   async logRequest(request: any): Promise<void> {
     const connection = await amqp.connect(this.rabbitMqUrl);
@@ -19,6 +19,7 @@ export class LoggingService {
 
     console.log(`[LoggingService] Sent request log: ${logMessage}`);
 
+    
     await channel.close();
     await connection.close();
   }
